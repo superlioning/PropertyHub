c7c73f21144bebda3b6d37731a7e947d9a1b6382
Wenjie Zhou <128197156+AlexandriaSea@users.noreply.github.com>
2024-11-19 00:37:31 -0500

    Remove User model, update Property and Agent table models , and modify AWS Connector

--- WebAPI/PropertyHubLibrary/PropertyHubLibrary/Models/Agent.cs
+++ WebAPI/PropertyHubLibrary/PropertyHubLibrary/Models/Agent.cs
@@ -1,10 +1,20 @@
-﻿namespace PropertyHubLibrary.Models
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace PropertyHubLibrary.Models
 {
+    [DynamoDBTable("Agent")]
     public class Agent
     {
-        public required string AgentId { get; set; }
-        public required string AgentName { get; set; }
-        public required string AgentCompanyName { get; set; }
-        public required string AgentPhone { get; set; }
+        // Partition Key, RECO registration number
+        [DynamoDBHashKey] 
+        public required string RegistrationNumber { get; set; }
+
+        // Mandatory fields for agent registration
+        public required string Name { get; set; }
+        public required string RegistrationCategory { get; set; }
+        public required string BrokerageTradeName { get; set; }
+        public required string BrokeragePhone { get; set; }
+        public required string BrokerageEmail { get; set; }
+        public required Address BrokerageAddress { get; set; }
     }
 }
