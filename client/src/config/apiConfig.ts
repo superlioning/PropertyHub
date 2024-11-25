db634472505c0f5f2d1fb930944b6506047f63cb
Wenjie Zhou <128197156+AlexandriaSea@users.noreply.github.com>
2024-11-25 18:03:57 -0500

    Hide base URL in the env file and upload sample data

--- client/src/config/apiConfig.ts
+++ client/src/config/apiConfig.ts
@@ -1,3 +1,7 @@
-const API_BASE_URL = 'localhost:8080';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+if (!API_BASE_URL) {
+    throw new Error('REACT_APP_API_BASE_URL is not defined in the environment variables');
+}
 
 export default API_BASE_URL;
